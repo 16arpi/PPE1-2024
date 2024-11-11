163d5bd0c8b285e27d5d773139a3014381c76e1e
César <cesarpichon@gmail.com>
2024-11-11 15:26:38 +0100

    Ajout script ex2

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -10,8 +10,18 @@ fi
 
 I=0
 
+echo "N" $'\t' "URL" $'\t' "Code HTTP" $'\t' "Encodage" $'\t' "Mots"
+
 while read -r line
 do
-	echo $I $line
+	# On ajoute à deux reprises xargs pour se débarasser des espaces inutiles
+
+	# Code HTTP
+	HTTP_CODE=$(curl -s -I "$line" | head -n 1 | cut -d ' ' -f 2)
+	# Encodage
+	ENCODING=$(curl -s -I "$line" | egrep -i '^content-type:' | egrep 'charset=[[:alnum:]-]+[:space:]' | cut -d '=' -f 2 | xargs)
+	# Comptage des mots
+	WORDS_COUNT=$(lynx -dump "$line" | egrep "\b[[:alnum:]]+\b" -o | wc -l | xargs)
+	echo $I$'\t'$line$'\t'$HTTP_CODE$'\t'$ENCODING$'\t'$WORDS_COUNT
 	((I++))
 done < $FILENAME
